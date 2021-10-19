32696123b683ef642b084f41bfd4dc057e5ce345
pearly-c <pearlchristian6@gmail.com>
2021-10-19 15:57:21 -0700

    project section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,11 +69,34 @@ modalCloses.forEach((modalClose) => {
     modalClose.addEventListener('click', () =>{
         modalViews.forEach((modalView) => {
             modalView.classList.remove('active-modal')
-        })
+        }) 
     })
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container',{
+    //cssMode: true,
+    //loop: true,
+
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 
 /*==================== TESTIMONIAL ====================*/
